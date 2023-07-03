5d7350c86385369cf7630db90c5456d4f7d053df
manick27 <nguewouom@gmail.com>
2023-07-03 03:44:08 +0200

    update

--- resources/views/blog/blog-details.blade.php
+++ resources/views/blog/blog-details.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('title')
-    Home Page |  TMT
+    Blog Details Page |  TMT
 @endsection
 
 @section('content')
--- resources/views/blog/blog.blade.php
+++ resources/views/blog/blog.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('title')
-    Home Page |  TMT
+    Blog Page |  TMT
 @endsection
 
 @section('content')
