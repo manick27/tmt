a1e55fd3a942d66079c8b864ef4258a76a1179dd
manick27 <nguewouom@gmail.com>
2023-07-03 02:10:39 +0200

    add template bootstrap for blog

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -52,7 +52,7 @@
   <header id="header" class="header fixed-top" data-scrollto-offset="0">
     <div class="container-fluid d-flex align-items-center justify-content-between">
 
-      <a href="index.html" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+      <a href="/home" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
         <!-- Uncomment the line below if you also wish to use an image logo -->
         <!-- <img src="assets/img/logo.png" alt=""> -->
         <h1>HeroBiz<span>.</span></h1>
@@ -61,21 +61,22 @@
       <nav id="navbar" class="navbar">
         <ul>
 
-          <li class="dropdown"><a href="#"><span>Home</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+          {{-- <li class="dropdown"><a href="#"><span>Home</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
-              <li><a href="index.html">Home 1 - index.html</a></li>
+              <li><a href="/home">Home 1 - /home</a></li>
               <li><a href="index-2.html">Home 2 - index-2.html</a></li>
               <li><a href="index-3.html" class="active">Home 3 - index-3.html</a></li>
               <li><a href="index-4.html">Home 4 - index-4.html</a></li>
             </ul>
-          </li>
+          </li> --}}
 
-          <li><a class="nav-link scrollto" href="index.html#about">About</a></li>
-          <li><a class="nav-link scrollto" href="index.html#services">Services</a></li>
-          <li><a class="nav-link scrollto" href="index.html#portfolio">Portfolio</a></li>
-          <li><a class="nav-link scrollto" href="index.html#team">Team</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li class="dropdown megamenu"><a href="#"><span>Mega Menu</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+          <li><a class="nav-link scrollto" href="/home#hero-fullscreen">Home</a></li>
+          <li><a class="nav-link scrollto" href="/home#about">About</a></li>
+          <li><a class="nav-link scrollto" href="/home#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="/home#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="/home#team">Team</a></li>
+          <li><a href="/blog">Blog</a></li>
+          {{-- <li class="dropdown megamenu"><a href="#"><span>Mega Menu</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
               <li>
                 <a href="#">Column 1 link 1</a>
@@ -98,8 +99,8 @@
                 <a href="#">Column 4 link 3</a>
               </li>
             </ul>
-          </li>
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+          </li> --}}
+          {{-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
             <ul>
               <li><a href="#">Drop Down 1</a></li>
               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
@@ -115,13 +116,13 @@
               <li><a href="#">Drop Down 3</a></li>
               <li><a href="#">Drop Down 4</a></li>
             </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="index.html#contact">Contact</a></li>
+          </li> --}}
+          <li><a class="nav-link scrollto" href="/home#contact">Contact</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle d-none"></i>
       </nav><!-- .navbar -->
 
-      <a class="btn-getstarted scrollto" href="index.html#about">Get Started</a>
+      <a class="btn-getstarted scrollto" href="/home#about">Get Started</a>
 
     </div>
   </header>
@@ -130,7 +131,7 @@
 
   @yield('content')
 
-  
+
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
 
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::view('/home', 'home');
+
+Route::view('/blog', 'blog/blog');
+
+Route::view('/blog-details', 'blog/blog-details');
