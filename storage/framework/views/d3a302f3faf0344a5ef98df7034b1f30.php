e2eb43fd3ef3e5c7f007a6464f3bee362f80e36a
manick27 <nguewouom@gmail.com>
2023-07-09 12:54:01 +0200

    update

--- storage/framework/views/d3a302f3faf0344a5ef98df7034b1f30.php
+++ storage/framework/views/d3a302f3faf0344a5ef98df7034b1f30.php
@@ -63,19 +63,11 @@
 
           
 
-<<<<<<< HEAD
-          <li><a class="nav-link scrollto" href="/home#hero-fullscreen"><?php echo e(__('Accueil')); ?></a></li>
-          <li><a class="nav-link scrollto" href="/home#about"><?php echo e(__('A propos')); ?></a></li>
-          <li><a class="nav-link scrollto" href="/home#services"><?php echo e(__('Services')); ?></a></li>
-          <li><a class="nav-link scrollto" href="/home#portfolio"><?php echo e(__('Portefeuille')); ?></a></li>
-          <li><a class="nav-link scrollto" href="/home#team"><?php echo e(__('Equipe')); ?></a></li>
-=======
           <li><a class="nav-link scrollto" href="/#hero-fullscreen"><?php echo e(__('Accueil')); ?></a></li>
           <li><a class="nav-link scrollto" href="/#about"><?php echo e(__('A propos')); ?></a></li>
           <li><a class="nav-link scrollto" href="/#services"><?php echo e(__('Services')); ?></a></li>
           <li><a class="nav-link scrollto" href="/#portfolio"><?php echo e(__('Portefeuille')); ?></a></li>
           <li><a class="nav-link scrollto" href="/#team"><?php echo e(__('Equipe')); ?></a></li>
->>>>>>> 0a0197ccb2cb830391daccc83c6edd84774f9c1b
           <li><a href="/blog"><?php echo e(__('Blog')); ?></a></li>
           <li><a class="nav-link scrollto" href="/#contact"><?php echo e(__('Contact')); ?></a></li>
 
